55c7a107018c5b9613816e93b6820ce15bec701d
Knut Holm <knut.holm28@gmail.com>
2019-01-03 14:58:18 +0100

    Updated to GitHub Pages hosting

--- publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BASE_PATH="${HOME}/blog"
-PUBLIC_HTML=/var/www/ivahol.cz/public_html
-VIRTUALENV="${HOME}/.virtualenvs/pelican"
-
-source $VIRTUALENV/bin/activate && cd $BASE_PATH && make html && cp -R output/. $PUBLIC_HTML
