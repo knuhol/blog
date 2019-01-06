d1d54b58112c45cbbe0696220c990f44dc0d32cd
Knut Holm <knut.holm28@gmail.com>
2019-01-06 19:45:58 +0100

    Updated name to Knut Holm

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Ivana Holm'
+AUTHOR = 'Knut Holm'
 SITENAME = 'O Švédsku a tak'
 SITETITLE = SITENAME
 SITESUBTITLE = AUTHOR
-SITEDESCRIPTION = 'Ivana Holm píše blog (nejen) o svém odchodu do Švédska'
+SITEDESCRIPTION = 'Knut Holm píše blog (nejen) o svém odchodu do Švédska'
 SITEURL = 'https://svedsko.blog'
 COPYRIGHT_YEAR = '2017-2019'
 CC_LICENSE = {'name': 'CC BY-SA 4.0', 'link': 'https://creativecommons.org/licenses/by-sa/4.0/deed.cs'}
@@ -21,7 +21,7 @@ GOOGLE_ANALYTICS = 'UA-110306205-1'
 PATH = 'content'
 STATIC_PATHS = ['images', 'files']
 
-TIMEZONE = 'Europe/Prague'
+TIMEZONE = 'Europe/Stockholm'
 DEFAULT_DATE_FORMAT = '%-d. %-m. %Y (%-H:%M)'
 
 I18N_TEMPLATES_LANG = 'en'
@@ -35,10 +35,9 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-SOCIAL = (('linkedin', 'https://www.linkedin.com/in/ivana-holm'),
-          ('github', 'https://github.com/akarienta'),
-          ('stack-overflow', 'https://stackoverflow.com/users/3780766'),
-          ('instagram', 'https://www.instagram.com/ivanaholm'))
+SOCIAL = (('linkedin', 'https://www.linkedin.com/in/knuhol'),
+          ('github', 'https://github.com/knuhol'),
+          ('stack-overflow', 'https://stackoverflow.com/users/3780766'))
 
 MENUITEMS = (('Archiv', '/archives.html'),
              ('Kategorie', '/categories.html'),
